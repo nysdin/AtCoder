a82aaecde60592285aeda1a0fe981cb959b82f88
ShiniYanada <40791830+ShiniYanada@users.noreply.github.com>
2021-05-02 17:05:02 +0900

    RE(簡略)

--- ABC/166/c.rb
+++ ABC/166/c.rb
@@ -1,26 +1,17 @@
-require 'set'
 n, m = gets.split.map(&:to_i)
 h = gets.split.map(&:to_i)
 ans = 0
-roots = Array.new(n){Set.new}
+t = Array.new(n, true)
 m.times do |i|
   a, b = gets.split.map(&:to_i)
-  roots[a-1].add(b-1)
-  roots[b-1].add(a-1)
+  if h[a-1] <= h[b-1]
+    t[a-1] = false
+  end
+  if h[b-1] <= h[a-1]
+    t[b-1] = false
+  end
 end
-c = 0
-while c < n
-  height= h[c]
-  flag = true
-  roots[c].each do |s|
-    if h[s] >= height
-      flag = false
-      break
-    end
-  end
-  if flag
-    ans += 1
-  end
-  c += 1
+t.each do |b|
+  ans += 1if b
 end
 puts ans
