9e399432a940c6b498cc2011276bb7e652217a81
ShiniYanada <40791830+ShiniYanada@users.noreply.github.com>
2021-05-02 21:03:23 +0900

    RE(dfs)

---
+++ ABC/165/c.rb
@@ -0,0 +1,33 @@
+$n, $m , q = gets.split.map(&:to_i)
+$ans = 0
+$seq = []
+q.times do |i|
+  a, b, c, d = gets.split.map(&:to_i)
+  $seq.push([a,b,c,d])
+end
+
+def calc(seq)
+  now = 0
+  $seq.each do |s|
+    if seq[s[1]-1] - seq[s[0]-1] == s[2]
+      now += s[3]
+    end
+  end
+  $ans = now if now > $ans
+end
+
+def dfs(seq)
+  if seq.length == $n
+    calc(seq)
+    return
+  end
+  val = seq[-1]
+  while val <= $m
+    seq.push(val)
+    dfs(seq)
+    val += 1
+    seq.pop
+  end
+end
+dfs([1])
+puts $ans
