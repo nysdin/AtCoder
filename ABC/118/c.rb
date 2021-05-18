8b78a9fe3029cfd236a6c417d28182a0319e4528
ShiniYanada <40791830+ShiniYanada@users.noreply.github.com>
2021-05-18 21:48:13 +0900

    正しい解法

--- ABC/118/c.rb
+++ ABC/118/c.rb
@@ -1,30 +1,7 @@
-require 'set'
 n = gets.to_i
 a = gets.split.map(&:to_i)
-a.sort!
-flag = false
-s = Set.new
-min = a[0]
-1.upto(n-1) do |i|
-  g = min.gcd(a[i])
-  if g == 1
-    flag = true
-  else
-    s.add(g)
-  end
+ans = 0
+n.times do |i|
+  ans = a[i].gcd(ans)
 end
- 
-if flag
-  puts 1
-else
-  gcds = s.to_a
-  len = gcds.length
-  ans = gcds[0]
-  len.times do |i|
-    (i+1).upto(len-1) do |j|
-      g = gcds[i].gcd(gcds[j])
-      ans = g if ans > g
-    end
-  end
-  puts ans
-end
+puts ans
