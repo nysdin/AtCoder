c19425daa546f9a9c970c6391f7540eb3eac8db2
nysdin <40791830+nysdin@users.noreply.github.com>
2021-05-31 15:54:42 +0900

    RE

--- ABC/110/c.rb
+++ ABC/110/c.rb
@@ -1,30 +1,18 @@
 s = gets.chomp
 t = gets.chomp
 len = s.length
-sh = Hash.new(0)
-th = Hash.new(0)
+sn = Array.new(26, -1)
+tn = Array.new(26, -1)
 len.times do |i|
-  sh[s[i]] += 1
-  th[t[i]] += 1
-end
-sn = Hash.new(0)
-tn = Hash.new(0)
-sh.each do |k, v|
-  sn[v] += 1
-end
-th.each do |k, v|
-  tn[v] += 1
-end
-sl = sh.length
-tl = th.length
-if sl != tl
-  puts 'No'
-else
-  sn.each do |k, v|
-    if tn[k] != v
+  a = s[i].ord - 97
+  b = t[i].ord - 97
+  if sn[a] != - 1 || tn[b] != -1
+    if sn[a] != b || tn[b] != a
       puts 'No'
       return
     end
   end
-  puts 'Yes'
+  sn[a] = b
+  tn[b] = a
 end
+puts 'Yes'
