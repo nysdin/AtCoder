ce5d1ac1fb14af38618a8b2e23d63463a4f6eb1f
nysdin <40791830+nysdin@users.noreply.github.com>
2021-06-05 16:20:52 +0900

    正解放

--- ABC/102/c.rb
+++ ABC/102/c.rb
@@ -1,37 +1,12 @@
 N = gets.to_i
-$aa = gets.split.map(&:to_i)
-l = -100
-r = 1000000001
+aa = gets.split.map(&:to_i)
+N.times do |i|
+  aa[i] -= (i+1)
+end
+aa.sort!
+b = aa[N/2]
 ans = 0
-def calc(b)
-  ans = 0
-  $aa.each_with_index do |a, i|
-    ans += (a-(b+i+1)).abs
-  end
-  return ans
+aa.each do |a|
+  ans += (b-a).abs
 end
- 
-while r-l != 1
-  mid = (l+r)/2
-  now = calc(mid)
-  pre = calc(mid-1)
-  nxt = calc(mid+1)
-  if pre >= now && now <= nxt
-    ans = now
-  end
-  
-  if now <= nxt
-    r = mid
-  else
-    l = mid
-  end
-end
-ll = calc(l)
-rr = calc(r)
-if ll > rr
-  ans = rr
-else
-  ans = ll
-end
-  
 puts ans
